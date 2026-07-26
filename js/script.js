449d75728362d060acfcd15554d1469b5601aa48
essence100 <essencebenedict@gmail.com>
2026-07-26 16:34:50 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log("Kamotas Website Loaded");
+// Mobile Menu Toggle
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+
+menuToggle.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+
+});
+
+
+
+// Close menu after clicking a link
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        navLinks.classList.remove("active");
+
+    });
+
+});
